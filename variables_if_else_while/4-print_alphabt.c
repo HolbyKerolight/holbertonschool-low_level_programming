b01b0ca75b5b391defe70bbe6b3684783cc2b699
HolbyKerolight <6726@holbertonstudents.com>
2023-06-20 08:31:03 -0700

    program that prints the alphabet in lowercase, followed by a new line

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -9,10 +9,11 @@ int main(void)
 {
 	int i;
 
-	for (i = 97; i < 123; i++) 
+	for (i = 97;i < 123; i++) 
 	{
 		if (i != 113 && i != 101)
 			putchar(i);
 	}
 	putchar('\n');
-	return (0);}
+	return (0);
+}
