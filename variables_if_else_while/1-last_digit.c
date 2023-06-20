cb3408c1cba91a9b8ff02f9005ff874a54702bd0
HolbyKerolight <6726@holbertonstudents.com>
2023-06-20 03:44:23 -0700

    Last digit of the random number stored in the variable n

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -16,11 +16,12 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if (n%10 < 6)
-		printf("Last digit of %d is less than 6\n", n);
-	else if (n%10 > 5)
-		printf("Last digit of %d is greater than 5\n", n);
+	m = n%10;
+	if (m < 6)
+		printf("Last digit of %d is %d and is less than 6\n", n,m);
+	else if (m > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n,m);
 	else
-		printf("Last digit of %d is 0\n", n);
+		printf("Last digit of %d is %d and is 0\n", n,m);
 	return (0);
 }
