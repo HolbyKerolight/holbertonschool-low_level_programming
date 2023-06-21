dc2385106476d440a02fb29543a7566dc278928d
HolbyKerolight <6726@holbertonstudents.com>
2023-06-21 02:07:55 -0700

    function that checks for lowercase character

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+/**
+*my_function - short description of my_function
+*@param1: description of param1
+*param2: description of param2
+*
+*Long description of my_function. This description should
+*provide detailed information about what the function does,
+*how it works, and any important details that the user
+*should know.
+*
+*Return: description of the return value
+*/
+int _islower(int c)
+{
+	return (c <= 97 && c >= 122);
+}
