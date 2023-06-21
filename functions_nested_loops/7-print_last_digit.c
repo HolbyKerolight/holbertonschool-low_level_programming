28946a590f5335e2b209d053e37e9187045e647c
HolbyKerolight <6726@holbertonstudents.com>
2023-06-21 06:58:59 -0700

    function that prints the last digit of a number

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+*_abs - function that computes the absolute value of an integer.
+*@n: The number to check
+*
+*Return: absolute value of n (int)
+*/
+#include "main.h"
+
+int _abs(int n)
+{
+	return _abs(n % 10);
+}
