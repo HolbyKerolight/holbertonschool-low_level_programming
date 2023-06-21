bcac6367535f852237a2d8fa3802abb3f1c49db5
HolbyKerolight <6726@holbertonstudents.com>
2023-06-21 02:19:38 -0700

    function that checks for lowercase character

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -2,6 +2,9 @@
 *_islower : function that checks for lowercase character.
 *@c: the character
 *
+*The standard library provides a similar function: islower.
+*Run man islower to learn more.
+*
 *Return: 1 (lower case) 0 (other)
 */
 int _islower(int c)
