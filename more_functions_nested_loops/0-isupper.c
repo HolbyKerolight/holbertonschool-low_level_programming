4da41fb8c80cd935ea0bc224ea1a0b89321d66dd
HolbyKerolight <6726@holbertonstudents.com>
2023-06-22 14:17:48 -0700

    function that checks for uppercase character

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+/**
+*_isupper - Checks for uppercase character
+*
+* @c : the value to check (int)
+*
+* Return : 1 (if c is uppercase) 0 (otherwise)
+*
+*/
+#include "main.h"
+
+int _isupper(int c)
+{
+	return (c <= 65 && c >= 90)
+}
