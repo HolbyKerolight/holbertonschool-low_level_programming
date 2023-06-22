cad4a4c13a152cd3cc7bc6bd34e33347a738f4a8
HolbyKerolight <6726@holbertonstudents.com>
2023-06-22 01:53:58 -0700

    function that prints the 9 times table, starting with 0

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -29,7 +29,8 @@ void times_table(void)
 			{
 				_putchar('0' + product / 10);
 			}
-			_putchar('0' + product % 10);
+			if (product != 0)
+				_putchar('0' + product % 10);
 			if (j != 9)
 			{
 				_putchar(',');
