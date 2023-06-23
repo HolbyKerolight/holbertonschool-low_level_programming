98185ea49951f20c180bcf2fe8c87891ce13fda5
HolbyKerolight <6726@holbertonstudents.com>
2023-06-23 13:47:39 -0700

    function that prints a triangle, followed by a new line

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -19,9 +19,13 @@ void print_triangle(int size)
 	}
 	else
 	{
-		for (i = 1; i < size; i++)
+		for (i = 1; i <= size; i++)
 		{
-			for (j = 1; j < size; j++)
+			for (k = 0; k < size - i; k++)
+			{
+				_putchar(' ');
+			}
+			for (j = 0; j < i; j++)
 			{
 				_putchar('#');
 			}
