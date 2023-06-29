57b45641e923fb7b7d090d2f45b852903334b236
HolbyKerolight <6726@holbertonstudents.com>
2023-06-29 04:27:27 -0700

    function that prints half of a string, followed by a new line

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+/**
+ * puts_half - function that prints half of a string, followed by a new line
+ *
+ * @str : pointer to a char
+ *
+ * Return: void
+ */
+#include <stdio.h>
+#include <string.h>
+
+void puts_half(char *str)
+{
+	int length = strlen(str), n;
+
+	n = (length % 2 == 0) ? length / 2 : (length -1) / 2;
+	for (int i = n; i < length; i++)
+		putchar(str[i]);
+	printf("\n");
+}
