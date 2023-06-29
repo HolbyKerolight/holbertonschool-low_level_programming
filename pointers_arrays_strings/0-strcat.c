7f46259c47a4332fbaaabeb23df9508cf05656b1
HolbyKerolight <6726@holbertonstudents.com>
2023-06-29 14:13:28 -0700

    function that concatenates two strings

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,19 @@
+/**
+ * _strcat - function that concatenates two strings
+ *
+ * @dest : une chaine de caractere
+ * @src : deuxieme chaine
+ *
+ * Return: la concatenation des deux chaines
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+	for (j = 0; src[j] != '\0'; j++)
+		dest[i + j] = src[j];
+	dest[i + j] = '\0';
+	return (dest);
+}
